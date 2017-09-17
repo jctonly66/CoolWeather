c939d50f321ef93e561f611b2cb8e19f370cc967
JCT <jctonly66@gmail.com>
2017-09-17 23:15:52 +0800

    dddddddddddddddddddddddd

--- app/src/main/java/study/jct/com/myapplication/ChooseAreaFragment.java
+++ app/src/main/java/study/jct/com/myapplication/ChooseAreaFragment.java
@@ -40,7 +40,7 @@ public class ChooseAreaFragment extends Fragment {
     public static final int LEVEL_CITY = 1;
     public static final int LEVEL_COUNTY = 2;
     private ProgressDialog progressDialog;
-    private TextView titleText branch dev;
+    private TextView titleText;
     private Button backButton;
     private ListView listView;
     private ArrayAdapter<String> adapter;
